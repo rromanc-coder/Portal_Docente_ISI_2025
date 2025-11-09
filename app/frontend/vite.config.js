5252258efebc0c5ea103672f4432ceafc1153b79
Ricardo Román <rrcuevas.uaemex@gmail.com>
2025-11-09 17:32:00 -0600

    Commit 4

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.',             // base del proyecto
+  publicDir: 'public',   // carpeta de archivos estáticos
+  build: {
+    outDir: 'dist',      // salida del build
+  },
   plugins: [react()],
-  server: { port: 3000, host: true }
 })
