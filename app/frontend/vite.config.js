243559f18b178870ff8a36b21236d84484dfe1a5
Ricardo Román <rrcuevas.uaemex@gmail.com>
2025-11-09 21:51:04 -0600

    Sinc15

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// ✅ Configuración para despliegue en Nginx (rutas relativas)
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ← importante para rutas relativas (evita /monitor mostrando el home)
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-})
+});
